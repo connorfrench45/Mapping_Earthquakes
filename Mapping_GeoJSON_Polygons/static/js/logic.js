5e82fbd3b16feb37f42e7c614d0fd87e6c4a407e
Connor French <connorfrench45@gmail.com>
2022-06-16 10:38:48 -0700

    rest of neighborhood code

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -20,8 +20,8 @@ let baseMaps= {
 
 // Create the map object with center, zoom level and default layer
 let map = L.map('mapid', {
-    center: [44, -80],
-    zoom: 3,
+    center: [43.7, -79.3],
+    zoom: 11,
     layers: [satelliteStreets]
 })
 
@@ -29,7 +29,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto neighborhoods GeoJSON URL
-let torontoHoods = ''
+let torontoHoods = 'https://raw.githubusercontent.com/connorfrench45/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json'
 
 // Create a style for the lines
 let myStyle = {
@@ -38,13 +38,8 @@ let myStyle = {
 }
 
 // Grabbing our GeoJSON data
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data
-    L.geoJSON(data, {
-        style: myStyle,
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup('<h3> Airline: ' + feature.properties.airline + '</h3> <hr> <h3> Destination: ' + feature.properties.dst + '</h3>');
-        }
-    }).addTo(map);
+    L.geoJSON(data).addTo(map);
 });
